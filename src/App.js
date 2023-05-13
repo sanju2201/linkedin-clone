ba27c7b85119319195a05a21af64432c2a65b7eb
KUMARI RITU <120951254+KM31RITU@users.noreply.github.com>
2023-05-13 23:37:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,51 +2,53 @@ import React, { useEffect } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
-import Login from './Loging'
 import Widget from './Widget';
+import Login from './Login';
 import { loginuser, logoutuser, selectUser } from './features/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import { auth } from "firebase"
-
+import { auth } from './firebase';
 
 function App() {
- const user = useSelector(selectUser);
- const dispatch = useDispatch();
- useEffect(() => {
-  auth.onAuthStateChanged((userAuth)=>{
-    if (userAuth) {
-      //already login
-      dispatch(loginuser({
-        email:userAuth.email,
-        uid:userAuth.uid,
-        photoURL:userAuth.photoURL,
-        displayName:userAuth.displayName
-      }))
-    }
-    else {
-      //logout
-      dispatch(logoutuser())
-    }
-  })
- },
- [])
-return (
-  <>
-  {
-  !user ? (<Login/>) : (
-<div className="app_wrapper">
-  <Header/>
-  <div className="app_body">
-    <Sidebar/>
-    <Feed/>
-    <Widget/>
-    </div>
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+useEffect(()=>{
+auth.onAuthStateChange((userAuth)=>{
+  if(userAuth) {
+    //login 
+    dispatch(loginuser({
+      email:userAuth.email,
+                uid:userAuth.uid,
+                photoURL:userAuth.photoURL,
+                displayName:userAuth.displayName
 
-  </div>
-  )
+          }))
+  }
+  else {
+    //logout
+    dispatch(logoutuser())
+  }
+
+})
+},[])
+  return (
+    <>
+    {
+      !user ? (<Login/>) : (
+        <div className="ap-wrapper">
+      <Header/>
+      <div className="app-body">
+        <Sidebar/>
+        <Feed/>
+        <Widget/>
+
+      </div>
+
+
+    </div>
+      )
 }
 </>
-);
+  );
 }
 
 export default App;
